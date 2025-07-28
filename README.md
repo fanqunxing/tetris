839af88c14578b999af130c6b9309b445ef143d5
fanqunxing <fanqunxing@qq.com>
2025-07-28 23:47:53 +0800

    feat: 初始化画布

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # tetris
+
+```sh
+make        # 编译
+make clean  # 删除编译产物
+```
--- src/tetris.c
+++ src/tetris.c
@@ -1,3 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int clear_screen() {
+  return system("clear");
+}
+
+int init_canvas(int width, int height, int border_width) {
+  printf("-----start----- \n");
+  width += 2 * border_width;
+  height += 2 * border_width;
+  for (int i = 0; i < height; i++) {
+    for (int j = 0; j < width; j++) {
+      if (i < border_width || i >= height - border_width ||
+          j < border_width || j >= width - border_width)
+        printf("#");
+      else
+        printf(" ");
+    }
+    printf("\n");
+  }
+  printf("-----end----- \n");
+  return 0;
+}
+
 int main () {
   printf("Tetris game starting...\n");
+  clear_screen();
+  init_canvas(20, 20, 1);
+  while(1) {
+
+  }
 }
