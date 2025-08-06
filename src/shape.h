1c4488825dc18d50555a7388e9fd57b278a6c3c9
fanqunxing <fanqunxing@qq.com>
2025-08-07 00:11:42 +0800

    feat: add shape mod

--- src/shape.h
+++ src/shape.h
@@ -24,7 +24,7 @@ typedef struct Shape
 {
   enum ShapeType type;
   enum ShapeRotation rotation;
-  int points[5][2]; // Max 5 points per shape
+  int points[5][2];
 } Shape;
 
 int shape_create(Shape **shape, enum ShapeType type, enum ShapeRotation rotation);
