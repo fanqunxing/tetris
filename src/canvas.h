582d63f5184429d1c66a9e3ac1463672c891c1df
fanqunxing <fanqunxing@qq.com>
2025-08-11 23:24:06 +0800

    style: 格式化

--- src/canvas.h
+++ src/canvas.h
@@ -14,7 +14,7 @@ typedef struct
   int width;
   int height;
   int border_width;
-  int** pixels;
+  int **pixels;
 } Canvas;
 
 int canvas_create(Canvas **canvas);
@@ -23,5 +23,5 @@ int canvas_init(Canvas *canvas, int width, int height, int border_width);
 int canvas_clear(Canvas *canvas);
 int canvas_add_points(Canvas *canvas, int points[5][2], int num_points);
 int canvas_remove_ponits(Canvas *canvas, int points[5][2], int num_points);
-int canvas_get_pixels(Canvas *canvas, int** pixels, int* x_size, int* y_size);
+int canvas_get_pixels(Canvas *canvas, int **pixels, int *x_size, int *y_size);
 int canvas_draw(Canvas *canvas);
