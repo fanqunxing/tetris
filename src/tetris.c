7e3a1b6baf91f13be0c3788de6a076cb403d2df3
fanqunxing <fanqunxing@qq.com>
2025-08-11 23:51:09 +0800

    feat: 补充get_pixels

--- src/tetris.c
+++ src/tetris.c
@@ -12,7 +12,13 @@ int main()
   canvas_init(canvas, 20, 10, 1);
   Shape *shape = NULL;
   shape_create(&shape, SHAPE_J, ROTATION_0);
-  while (1)
+
+  int *pixels = NULL;
+  int x_size = 0;
+  int y_size = 0;
+  canvas_get_pixels(canvas, &pixels, &x_size, &y_size);
+  printf("x = %d y = %d \n", x_size, y_size);
+  while (0)
   {
     usleep(100000);
     canvas_add_points(canvas, shape->points, 4);
